1872026ff08c194ae6b339fb3a37cadb97a49458
Ishaan Gupta <ishaangupta999@gmail.com>
2026-03-05 17:18:00 +0530

    updated CVEs

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ const nextConfig: NextConfig = {
   // Production optimizations
   compress: true,
   poweredByHeader: false,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  
-  
   // Security headers
   async headers() {
     return [
